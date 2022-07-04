97b53af7b1ed45e48fef06d2e235b92b7371dd16
mariannarossim <100978790+mariannarossim@users.noreply.github.com>
2022-07-04 20:32:56 -0300

    Add files via upload

---
+++ provalp/src/Matriz.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+class Main {
+       
+        public static void main(String[] args) {
+        	
+        }
+            int[] inp = { 1, 2, 3, 4, 5 };
+            int n = inp.length;
+            
+                int l = array.length;
+                for (int i = 0; i < l; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        System.out.print(" " + array[i][j]);
+                    }
+                    System.out.println();
+                }
+                int m = array1.length;
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        System.out.print(" " + array1[i][j]);
+                    }
+                    System.out.println();
+                }
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j <= 4; j++) {
+                        System.out.print(" " + (array[i][j] / array1[i][j]));
+                    }
+                    System.out.println(" ");
+                }
+            }
+        
+            
+        
+    
+
+
+
+
+
+
+
+
+
+
+
